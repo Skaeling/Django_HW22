4f14fcd07e3e8c568324d3d3d6ceeb3263d6fc38
Lana Virina <lana.virina@gmail.com>
2024-10-31 19:43:45 +0300

    fix

--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     description = models.TextField(verbose_name="Описание")
 
     def __str__(self):
-        return f'{self.name} ({self.description})'
+        return self.name
 
     class Meta:
         verbose_name = 'категория'
@@ -17,14 +17,14 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name="Товар")
     description = models.TextField(null=True, blank=True, verbose_name="Описание")
-    image = models.ImageField(upload_to='photos/', verbose_name="Фотография")
+    image = models.ImageField(upload_to='photos/', blank=True, null=True, verbose_name="Фотография")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     price = models.IntegerField(default='null', verbose_name='Стоимость')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
 
     def __str__(self):
-        return f'{self.name} {self.category} {self.price})'
+        return f'{self.name} {self.category} {self.price}'
 
     class Meta:
         verbose_name = 'товар'
