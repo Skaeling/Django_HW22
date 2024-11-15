cfb5ef493b10373d5b1c2c824d7503b882e151d3
Lana Virina <lana.virina@gmail.com>
2024-11-15 04:42:24 +0300

    fix catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,8 +4,8 @@ from .views import HomeListView, ProductDetailView, ContactCreateView, ProductCr
 app_name = 'catalog'
 
 urlpatterns = [
-    path('home/', HomeListView.as_view(), name='home'),
-    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('contact/', ContactCreateView.as_view(), name='contact'),
-    path('add_product/', ProductCreateView.as_view(), name='add_product'),
+    path('', HomeListView.as_view(), name='home'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('contacts/', ContactCreateView.as_view(), name='contact'),
+    path('products/new/', ProductCreateView.as_view(), name='add_product'),
 ]
