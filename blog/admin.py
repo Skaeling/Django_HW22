cdb0a8e7296cf7352902f68b9e881beaa98f539f
Lana Virina <lana.virina@gmail.com>
2024-11-15 04:40:55 +0300

    fix model

--- blog/admin.py
+++ blog/admin.py
@@ -4,5 +4,5 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'created_at', 'updated_at']
-    list_filter = ['title', 'created_at', 'updated_at']
+    list_display = ['id', 'title', 'created_at', 'is_published', 'view_count']
+    list_filter = ['title', 'created_at', 'is_published', 'view_count']
