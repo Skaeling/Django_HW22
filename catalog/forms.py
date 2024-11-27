081689112efdc537c2a7b9f13ea2b3dd9a8b74c7
Lana Virina <lana.virina@gmail.com>
2024-11-27 20:40:48 +0300

    add Product form validation

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,20 +1,39 @@
 from django.forms import ModelForm
-from .models import Category, Product, Contact
+from .models import Product, Contact
+from django.core.exceptions import ValidationError
 
-
-class CategoryForm(ModelForm):
-    class Meta:
-        model = Category
-        fields = ["name", "description"]
+PROHIBITED_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "description", "image", "category", "price"]
+        exclude = ('created_at', 'updated_at',)
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        for word in PROHIBITED_WORDS:
+            if word in name.lower():
+                raise ValidationError(f'Использовано запрещенное слово "{word}", введите другое название')
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        for word in PROHIBITED_WORDS:
+            if word in description.lower():
+                raise ValidationError(f'Использовано запрещенное слово "{word}", откорректируйте описание')
+        return description
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price < 0:
+            raise ValidationError('Цена не может быть отрицательной')
+        elif price == 0:
+            raise ValidationError('Цена не может быть равна нулю')
+        return price
 
 
 class ContactForm(ModelForm):
     class Meta:
         model = Contact
-        fields = ["name", "email", "message"]
+        fields = "__all__"
--- catalog/models.py
+++ catalog/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name="Наименование товара")
     description = models.TextField(null=True, blank=True, verbose_name="Описание")
-    image = models.ImageField(upload_to='photos/', blank=True, null=True, verbose_name="Фотография")
+    image = models.ImageField(upload_to='photos/', default='photos/placeholder-500x500.gif', verbose_name="Фотография")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name="Категория")
     price = models.IntegerField(default='null', verbose_name='Стоимость')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
